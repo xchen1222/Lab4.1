20940bcf8d5de04b4cf45f1dec4e7c6a281912c4
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2018-03-14 09:22:20 -0400

    Card

--- Lab4.1/src/Card.java
+++ Lab4.1/src/Card.java
@@ -1,4 +1,67 @@
 
 public class Card {
 
+	String[] rank ; 
+	String[] suit ;
+	int[] value ; 
+	
+	
+	
+	public Card (String[] rank , String[] suit , int[] value) {
+		this.rank = rank ; 
+		this.suit = suit ; 
+		this.value = value; 
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Challenge 1: Card Class
+	Fields:
+		1. String rank(name of the card)
+		2. String suit
+		3. int pointValue
+		
+	Methods:
+		1. Constructor (String rank, String suit, int pointValue)
+		2. Getters for all three fields.
+		3. public boolean equals(Card otherCard) //check if this card equals
+		another in value.
+		4. Public String toString() - return an appropriate string for this card
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- Lab4.1/src/Runner.java
+++ Lab4.1/src/Runner.java
@@ -2,7 +2,8 @@
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Card c1 = new Card ("J" , "Hearts" , 11) ; 
 
 	}
 
